3ee98edd4bc7bfb22644449bbddadaad0ba58dbf
Mati <kargo90@wp.pl>
2019-11-15 14:19:07 +0100

    lambda filter color ;p

--- src/main/java/pl/mpas/git_examples/car/service/CarFilter.java
+++ src/main/java/pl/mpas/git_examples/car/service/CarFilter.java
@@ -2,10 +2,29 @@ package pl.mpas.git_examples.car.service;
 
 import pl.mpas.git_examples.car.model.Car;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CarFilter {
     public List<Car> findCarOfColor(List<Car> cars, String color){
-        return null;
+        List<Car> carcolor= new ArrayList<Car>();
+
+        for(Car car:cars){
+            if(car.getColor().equals(color)){
+                carcolor.add(car);
+            }
+        }
+        return carcolor;
+
+
+    }
+    public List<Car> findCarOfColorJava8(List<Car> cars, String color){
+       cars.stream()
+               .filter(car -> car.getColor().equals(color));
+
+        return Collections.emptyList();
+
+
     }
 }
