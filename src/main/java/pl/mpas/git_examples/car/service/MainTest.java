6bd0d050f5113677e0bf871341b605bf44588cc1
Mati <kargo90@wp.pl>
2019-11-15 14:02:07 +0100

    after calculation lambdas

--- src/main/java/pl/mpas/git_examples/car/service/MainTest.java
+++ src/main/java/pl/mpas/git_examples/car/service/MainTest.java
@@ -1,6 +1,7 @@
 package pl.mpas.git_examples.car.service;
 
 public class MainTest {
+    public static void main(String[] args) {
     LambdaConverter my=new LambdaConverter() {
         @Override
         public int convertStringToInt(String string) {
@@ -9,6 +10,11 @@ public class MainTest {
     };
 
     LambdaConverter my2=one->one.length();
-    Interface2Int dodawanie=(w,e)->(w+e);
-    Interface2Int mnozenie=(a,b)->(a*b);
+
+
+
+        Interface2Int dodawanie=(w,e)->w+e;
+        Interface2Int mnozenie=(a,b)->a*b;
+        System.out.println(dodawanie.ints2(1,2));
+    }
 }
