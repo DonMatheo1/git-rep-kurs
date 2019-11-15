447b279b890bf499c3c530b4dc4abc1c21c5bd54
Mati <kargo90@wp.pl>
2019-11-15 11:37:17 +0100

    model car

---
+++ src/main/java/pl/mpas/git_examples/car/model/Car.java
@@ -0,0 +1,18 @@
+package pl.mpas.git_examples.car.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+
+@Data
+public class Car {
+    //poczytać o hashcodach kontraktach
+    private String brand;
+    private String model;
+    private String color;
+    private BigDecimal price;
+    private int engine;
+
+
+}
