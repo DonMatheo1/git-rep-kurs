e7a8f856e7c1e6fc5e9e0201a67e2a87b8d80228
Mati <kargo90@wp.pl>
2019-11-15 15:38:35 +0100

    changes on Hello

--- src/main/java/pl/mpas/git_examples/Hello.java
+++ src/main/java/pl/mpas/git_examples/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello git");
 
-        System.out.println("Hello my friend");
+        System.out.println("Hello my friend!!!");
 
         System.out.println("Hello my friend again");
     }
